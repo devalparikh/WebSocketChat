fd5da8d2eed18cedefb7565fea67a6e1ea013f79
Deval Parikh <devalpp@gmail.com>
2018-08-06 10:39:15 -0400

    Chat message sockets working

--- server.js
+++ server.js
@@ -13,3 +13,23 @@ console.log('Server running....')
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+// Emmit events go here
+io.sockets.on('connection', function(socket){
+  // Connect
+  // Array of connections being pushed to socket
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length)
+
+  // Disconnect
+  socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  // Send message
+  socket.on('send message', function(data){
+    // console.log(data)
+    io.sockets.emit('new message', {msg: data});
+  });
+});
